008094ca2a666784eb8d46d1ff279bf497bf7817
VladaBerlin <71773673+VladaBerlin@users.noreply.github.com>
2021-12-19 23:46:27 +0300

    Add files via upload

--- myapp.py
+++ myapp.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template, request, redirect, url_for
 from sqlalchemy import func
 from flask_sqlalchemy import SQLAlchemy
-import collections
+import sqlite3
+import plotly
+import plotly.express as px
+import json
+import pandas as pd
+#import plotly.graph_objects as go
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///lang_base.db'
 db = SQLAlchemy(app)
+
 
 class Person(db.Model):
     __tablename__ = 'people'
@@ -35,14 +41,17 @@ class Answers(db.Model):
     q7 = db.Column(db.Text)
     q8 = db.Column(db.Text)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/questions')
 def question_page():
     questions = Questions.query.all()
     return render_template('questions.html', questions=questions)
+
 
 @app.route('/process', methods=['get'])
 def answer_process():
@@ -62,7 +71,6 @@ def answer_process():
     )
     db.session.add(person)
     db.session.commit()
-    db.session.refresh(person)
     q1 = request.args.get('sceng')
     q2 = request.args.get('scger')
     q3 = request.args.get('scfr')
@@ -71,42 +79,108 @@ def answer_process():
     q6 = request.args.get('unger')
     q7 = request.args.get('unfr')
     q8 = request.args.get('unesp')
-    answer = Answers(id=person.id, q1=q1, q2=q2, q3=q3, q4=q4,
+    answer = Answers(q1=q1, q2=q2, q3=q3, q4=q4,
                      q5=q5, q6=q6, q7=q7, q8=q8)
     db.session.add(answer)
     db.session.commit()
-    return 'Ok'
+    return render_template('thanks.html')
+
 
 @app.route('/statistics')
 def stats():
-    all_info = {}
-    age_stats = db.session.query(
-        func.avg(Person.age),
-        func.min(Person.age),
-        func.max(Person.age)
-    ).one()
-    all_info['age_mean'] = age_stats[0]
-    all_info['age_min'] = age_stats[1]
-    all_info['age_max'] = age_stats[2]
-    all_info['total_count'] = Person.query.count()
-    all_info['city_max'] = db.session.query(list(collections.Counter(Person.city).keys())[0])
-    all_info['q1_max'] = db.session.query(list(collections.Counter(Answers.q1).keys())[0])
-    q1_answers = db.session.query(Answers.q1).all()
-    all_info['q2_mean'] = db.session.query(list(collections.Counter(Answers.q2).keys())[0])
-    q2_answers = db.session.query(Answers.q2).all()
-    all_info['q3_mean'] = db.session.query(list(collections.Counter(Answers.q3).keys())[0])
-    q3_answers = db.session.query(Answers.q3).all()
-    all_info['q4_mean'] = db.session.query(list(collections.Counter(Answers.q4).keys())[0])
-    q4_answers = db.session.query(Answers.q4).all()
-    all_info['q5_mean'] = db.session.query(list(collections.Counter(Answers.q5).keys())[0])
-    q5_answers = db.session.query(Answers.q5).all()
-    all_info['q6_mean'] = db.session.query(list(collections.Counter(Answers.q6).keys())[0])
-    q6_answers = db.session.query(Answers.q6).all()
-    all_info['q7_mean'] = db.session.query(list(collections.Counter(Answers.q7).keys())[0])
-    q7_answers = db.session.query(Answers.q7).all()
-    all_info['q8_mean'] = db.session.query(list(collections.Counter(Answers.q8).keys())[0])
-    q8_answers = db.session.query(Answers.q8).all()
-    return render_template('statistics.html', all_info=all_info)
+    con = sqlite3.connect('lang_base.db')
+    cur = con.cursor()
+    query = '''
+    SELECT AVG(age)
+    FROM people'''
+    cur.execute(query)
+    res = cur.fetchone()
+    avg_age = res[0]
+
+    city_query = '''
+    SELECT city
+    FROM people'''
+    cur.execute(city_query)
+    cities = cur.fetchall()
+    lst_city = []
+    lst_city_counter = []
+    for element in cities:
+        lst_city.append(element[0])
+        lst_city_counter.append(1)
+    df_city = pd.DataFrame(
+        {
+            'city': lst_city,
+            'amount': lst_city_counter
+        }
+    )
+    fig1 = px.histogram(df_city, x='city', y='amount', title='Статистика по городам')
+    graph1 = json.dumps(fig1, cls=plotly.utils.PlotlyJSONEncoder)
+
+    query_q = '''
+    SELECT q1, q2, q3, q4, q5, q6, q7, q8
+    FROM answers'''
+    cur.execute(query_q)
+    answers = cur.fetchall()
+    lst_q1 = []
+    lst_q2 = []
+    lst_q3 = []
+    lst_q4 = []
+    lst_q5 = []
+    lst_q6 = []
+    lst_q7 = []
+    lst_q8 = []
+    q_count = []
+    print(answers)
+    for ans in answers:
+        lst_q1.append(ans[0])
+        lst_q2.append(ans[1])
+        lst_q3.append(ans[2])
+        lst_q4.append(ans[3])
+        lst_q5.append(ans[4])
+        lst_q6.append(ans[5])
+        lst_q7.append(ans[6])
+        lst_q8.append(ans[7])
+        q_count.append(1)
+    df_q = pd.DataFrame(
+        {
+            'q1': lst_q1,
+            'q2': lst_q2,
+            'q3': lst_q3,
+            'q4': lst_q4,
+            'q5': lst_q5,
+            'q6': lst_q6,
+            'q7': lst_q7,
+            'q8': lst_q8,
+            'amount': q_count,
+        }
+    )
+    fig2 = px.bar(df_q, x=['q1', 'q2', 'q3', 'q4', 'q5', 'q6', 'q7', 'q8'],
+                  y='amount', title='Статистика по вопросам')
+    graph2 = json.dumps(fig2, cls=plotly.utils.PlotlyJSONEncoder)
+    return render_template('statistics.html', avg_age=avg_age,
+                           graph_city=graph1, graph_q=graph2)
+
+
+@app.route('/person/<person_id>')
+def person_page(person_id):
+    person = Person.query.get(person_id)
+    person_id = int(person_id)
+    con = sqlite3.connect('lang_base.db')
+    cur = con.cursor()
+    cur.execute('SELECT MAX(id) FROM people')
+    last_id = cur.fetchone()[0]
+    print(last_id)
+    if person_id + 1 > last_id:
+        person_next = ''
+    else:
+        person_next = '/person/' + str(person_id + 1)
+    if person_id == 1:
+        person_prev = ''
+    else:
+        person_prev = '/person/' + str(person_id - 1)
+    return render_template('person.html', person=person,
+                           person_next=person_next, person_prev=person_prev)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
